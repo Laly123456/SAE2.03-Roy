af6d26644b1abc354deeaa2ca414fb6ab57ef667
Roy Laly <roy80@mmi-dev.unilim.fr>
2026-04-22 13:49:55 +0200

    update credential

--- server/model.php
+++ server/model.php
@@ -14,9 +14,9 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "morap01");
-define("DBLOGIN", "morap01");
-define("DBPWD", "morap01");
+define("DBNAME", "roy80");
+define("DBLOGIN", "roy80");
+define("DBPWD", "roy80");
 
 
 function getAllMovies(){
