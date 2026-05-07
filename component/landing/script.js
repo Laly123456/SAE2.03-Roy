b51d0e805ac50133bcd4cce263e36a304647bfa3
Roy Laly <roy80@mmi-dev.unilim.fr>
2026-05-07 13:01:39 +0200

    iteration surprise

--- component/landing/script.js
+++ component/landing/script.js
@@ -1,7 +1,7 @@
 export const landing = {
     format: async function() {
         try {
-            const response = await fetch('./component/landing/template.html');
+            const response = await fetch('./component/landing/template.html?v=' + Date.now());
             if (!response.ok) return "Erreur : Fichier template non trouvé";
             return await response.text();
         } catch (error) {
